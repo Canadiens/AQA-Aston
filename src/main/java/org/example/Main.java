78214a75eb57bacac232b50eaa93365d957a0e64
Canadiens <xramoi91@mail.ru>
2023-12-17 15:37:34 +0300

    Lesson_2

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Lesson_2.printThreeWords();
+        Lesson_2.checkSumSing();
+        Lesson_2.printColor();
+        Lesson_2.compareNumber();
+        }
+    }
