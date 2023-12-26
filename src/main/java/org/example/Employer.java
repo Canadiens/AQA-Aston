0042c1928b50dc730730aeef8a83a22a2e1b7965
Canadiens <xramoi91@mail.ru>
2023-12-26 19:44:09 +0300

    Lesson_3

--- src/main/java/org/example/Employer.java
+++ src/main/java/org/example/Employer.java
@@ -1,25 +1,24 @@
 package org.example;
 public class Employer {
-   private String name;
-   private String position;
-   private String email;
-   private int phoneNumber;
-   private double salary;
-   private int age;
-
-   public Employer(String name,  String position, String email, int phoneNumber, double salary, int age ) {
-      this.name = name;
-      this.position = position;
-      this.email = email;
-      this.phoneNumber = phoneNumber;
-      this.salary = salary;
-      this.age = age;
-   }
-   public String toString() {
-      return "Name: " + name + "\nPosition: " + position + "\nEmail: " + email +
-              "\nPhone Number: " + phoneNumber + "\nSalary: " + salary + "\nAge: " + age;
-   }
-   public static void printInformation(Employer employer){
-      System.out.println(employer);
-   }
+    private final String name;
+    private final String position;
+    private final String email;
+    private final int phoneNumber;
+    private final double salary;
+    private final int age;
+    public Employer(String name, String position, String email, int phoneNumber, double salary, int age) {
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.age = age;
+    }
+    public String toString() {
+        return "Name: " + name + "\nPosition: " + position + "\nEmail: " + email +
+                "\nPhone Number: " + phoneNumber + "\nSalary: " + salary + "\nAge: " + age;
+    }
+    public static void printInformation(Employer employer) {
+        System.out.println(employer);
+    }
 }
--- src/main/java/org/example/Park.java
+++ src/main/java/org/example/Park.java
@@ -1,12 +1,10 @@
 package org.example;
 
 public class Park {
-
     private class Attractions {
-        private String attractionName;
-        private String operationTimes;
-        private double cost;
-
+        private final String attractionName;
+        private final String operationTimes;
+        private final double cost;
         public Attractions(String attractionName, String operationTimes, double cost) {
             this.attractionName = attractionName;
             this.operationTimes = operationTimes;
