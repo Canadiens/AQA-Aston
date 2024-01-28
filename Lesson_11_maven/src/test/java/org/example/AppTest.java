23e77c6b4133afe252ab69b977395fb344d6baa4
Siarhei <57797059+Canadiens@users.noreply.github.com>
2024-01-28 12:46:38 +0300

    Delete Lesson_11_maven/src directory

--- Lesson_11_maven/src/test/java/org/example/AppTest.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-public class AppTest {
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
-    }
-}
