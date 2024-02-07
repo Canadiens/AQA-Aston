db906935fb4282c96dc800febee6c431291351e0
Canadiens <xramoi91@mail.ru>
2024-02-07 22:34:37 +0300

    Lesson_14

--- Lesson_13/src/test/java/org/example/MtsTest.java
+++ Lesson_13/src/test/java/org/example/MtsTest.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -9,6 +10,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -19,6 +21,7 @@ public class MtsTest {
     @BeforeEach
     public void setUp() {
         driver = new EdgeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
 
     @AfterEach
@@ -31,24 +34,64 @@ public class MtsTest {
     @Test
     public void testMts() {
         driver.get("https://mts.by");
-
-        WebElement nameBlock = driver.findElement(By.xpath("//h2[contains(text(),'Онлайн пополнение ')]"));
-        assertTrue(nameBlock.isDisplayed());
-
-        List<WebElement> logos = driver.findElements(By.xpath("//div[@class='pay__partners']//img[@alt='Visa' or @alt='Verified By Visa' or @alt='MasterCard' or @alt='MasterCard Secure Code' or @alt='Белкарт' or @alt='МИР']"));
-        assertEquals(6, logos.size());
-
         WebElement cookiesButton = driver.findElement(By.xpath("//button[contains(@class,'cookie__cancel')][text()='Отклонить']"));
         cookiesButton.click();
         WebElement phoneNumber = driver.findElement(By.xpath("//*[@id='connection-phone']"));
         WebElement sumInput = driver.findElement(By.xpath("//*[@id='connection-sum']"));
         WebElement emailInput = driver.findElement(By.xpath("//*[@id='connection-email']"));
         WebElement button = driver.findElement(By.xpath("//*[@id='pay-connection']/button[contains(text(),'Продолжить')]"));
-        WebElement link = driver.findElement(By.xpath("//a[@href='/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/']"));
         phoneNumber.sendKeys("297777777");
         sumInput.sendKeys("10");
         emailInput.sendKeys("get@yandex.ru");
         button.click();
-        driver.get(link.getAttribute("href"));
+
+        driver.switchTo().frame(driver.findElement(By.xpath("//iframe[contains(@class, 'bepaid-iframe')]")));
+
+        WebElement payment = driver.findElement((By.xpath("//p[@class='header__payment-amount']")));
+        String actualPaymentAmount = payment.getAttribute("textContent").trim();
+        String expectedPaymentAmount = "10.00 BYN";
+        Assertions.assertEquals(expectedPaymentAmount, actualPaymentAmount);
+
+        WebElement button2 = driver.findElement(By.xpath("//button[contains(@class, 'colored')]"));
+        String buttonText = button2.getAttribute("textContent").trim();
+        Assertions.assertEquals("Оплатить  10.00 BYN", buttonText);
+
+        WebElement paymentInfoElement = driver.findElement(By.cssSelector("p.header__payment-info"));
+        String paymentInfoText = paymentInfoElement.getAttribute("textContent").trim();
+        String phone = "375297777777";
+        assertTrue(paymentInfoText.contains(phone));
+
+        checkTextPay("Имя держателя (как на карте)");
+        checkTextPay("Номер карты");
+        checkTextPay("Срок действия");
+        checkTextPay("CVC");
+
+        List<WebElement> iconElements = driver.findElements(By.cssSelector(".cards-brands img"));
+        assertEquals(5, iconElements.size());
+
+        driver.switchTo().parentFrame();
+
+        checkText("pay-connection");
+        checkText("pay-internet");
+        checkText("pay-instalment");
+        checkText("pay-arrears");
+    }
+
+    private void checkTextPay(String label) {
+        WebElement labelElement = driver.findElement(By.xpath("//label[contains(text(),'" + label + "')]"));
+        String labelText = labelElement.getAttribute("textContent").trim();
+        Assertions.assertEquals(label, labelText);
+    }
+
+    private void checkText(String formId) {
+        WebElement form = driver.findElement(By.id(formId));
+
+        List<WebElement> textFields = form.findElements(By.tagName("input"));
+        for (WebElement inputField : textFields) {
+            String placeholder = inputField.getAttribute("placeholder");
+            if (!placeholder.isEmpty()) {
+                assertTrue(inputField.isEnabled());
+            }
+        }
     }
 }
